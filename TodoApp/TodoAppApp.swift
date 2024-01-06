bf94d8a9fa8190206063bbebb75ff16389efc617
Mukund-Vaghasiya13 <mukundvaghasiya138@gmail.com>
2024-01-06 17:17:13 +0530

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Mukund vaghasiya  on 06/01/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
